16be642b066c9a2f8a750bc4b5619de19a98cfbe
holdn2 <yoocy01@gmail.com>
2025-10-26 01:52:36 +0900

    fix(regex, error): 허용문자에 쉼표 추가

--- src/constant/error.js
+++ src/constant/error.js
@@ -1,6 +1,6 @@
 export const ERROR_MESSAGE = {
   INVALID_CAR_NAME_CHARACTER:
-    '[ERROR] 자동차 이름은 영어, 한글, 숫자만 입력 가능합니다.',
+    '[ERROR] 자동차 이름은 영어, 한글, 숫자, 쉼표(,)만 입력 가능합니다.',
   INVALID_CAR_NAME_LENGTH: '[ERROR] 이름은 5자 이하로만 입력 가능합니다.',
   DUPLICATE_CAR_NAME:
     '[ERROR] 이름이 중복되었습니다. 모두 다른 이름으로 입력해주세요.',
